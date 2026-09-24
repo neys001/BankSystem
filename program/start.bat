04ba42ce5e7dedb5ebb04402b78a599d57f60dc1
neys001 <ppp-ppp-1963@mail.ru>
2026-09-24 20:59:06 +0300

    Initial commit

---
+++ program/start.bat
@@ -0,0 +1,4 @@
+
+@echo off
+start javaw --module-path "javafx-sdk-25\lib" --add-modules javafx.controls,javafx.fxml -jar "demo3.jar"
+
---
+++ program/start2.bat
@@ -0,0 +1,1 @@
+cmd /k "java --module-path "javafx-sdk-25\lib" --add-modules javafx.controls,javafx.fxml -jar demo3.jar"
